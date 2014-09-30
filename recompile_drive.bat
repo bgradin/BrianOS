82ec78ca34829535399031d4bc1b0caa4851c52e
gradinafrica <gradinafrica@yahoo.com>
2014-09-30 00:34:30 -0700

    Modified the drive recompilation script to refresh the drive in the VM

--- recompile_drive.bat
+++ recompile_drive.bat
@@ -1,5 +1,8 @@
 @echo off
 if exist boot_drive.vdi del /F boot_drive.vdi
 nasm boot_sector.asm -f bin -o boot_sector.bin
-VBoxManage convertdd boot_sector.bin boot_drive.vdi --format VDI
+vboxmanage convertdd boot_sector.bin boot_drive.vdi --format VDI
+vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium none
+vboxmanage closemedium disk boot_drive.vdi
+vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium boot_drive.vdi --type hdd
 pause
