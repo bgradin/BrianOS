19e3dc1c146dc360940a8a70fc2c0d8b64c90a06
gradinafrica <gradinafrica@yahoo.com>
2014-10-01 23:04:39 -0700

    Modified the compilation scripts to take an argument

--- recompile_drive.bat
+++ recompile_drive.bat
@@ -1,8 +1,15 @@
 @echo off
+if "%1"=="" goto no_argument
 if exist boot_drive.vdi del /F boot_drive.vdi
-nasm boot_sector.asm -f bin -o boot_sector.bin
+cd ..
+nasm %1 -f bin -o boot_sector.bin
 vboxmanage convertdd boot_sector.bin boot_drive.vdi --format VDI
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium none
 vboxmanage closemedium disk boot_drive.vdi
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium boot_drive.vdi --type hdd
+pause
+exit
+
+no_argument:
+echo No argument specified. Exiting.
 pause
--- recompile_run_drive.bat
+++ recompile_run_drive.bat
@@ -1,8 +1,15 @@
 @echo off
+if "%1"=="" goto no_argument
 if exist boot_drive.vdi del /F boot_drive.vdi >nul 2>&1
+cd ..
 nasm boot_sector.asm -f bin -o boot_sector.bin >nul 2>&1
 vboxmanage convertdd boot_sector.bin boot_drive.vdi --format VDI >nul 2>&1
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium none >nul 2>&1
 vboxmanage closemedium disk boot_drive.vdi >nul 2>&1
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium boot_drive.vdi --type hdd >nul 2>&1
 vboxmanage startvm BrianOS >nul 2>&1
+exit
+
+no_argument:
+echo No argument specified. Exiting.
+pause
