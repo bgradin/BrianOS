f3e36d7e0aea235aeb256659f3fdd76bd7e7f8ad
gradinafrica <gradinafrica@yahoo.com>
2014-10-01 23:18:27 -0700

    Corrected recompilation scripts

--- recompile_drive.bat
+++ recompile_drive.bat
@@ -1,8 +1,8 @@
 @echo off
 if "%1"=="" goto no_argument
-if exist boot_drive.vdi del /F boot_drive.vdi
 if not exist bin\NUL mkdir bin >nul 2>&1
 cd bin
+if exist boot_drive.vdi del /F boot_drive.vdi
 nasm %1 -f bin -o boot_sector.bin
 vboxmanage convertdd boot_sector.bin boot_drive.vdi --format VDI
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium none
--- recompile_run_drive.bat
+++ recompile_run_drive.bat
@@ -1,8 +1,8 @@
 @echo off
 if "%1"=="" goto no_argument
-if exist boot_drive.vdi del /F boot_drive.vdi >nul 2>&1
 if not exist bin\NUL mkdir bin >nul 2>&1
 cd bin
+if exist boot_drive.vdi del /F boot_drive.vdi >nul 2>&1
 nasm %1 -f bin -o boot_sector.bin >nul 2>&1
 vboxmanage convertdd boot_sector.bin boot_drive.vdi --format VDI >nul 2>&1
 vboxmanage storageattach BrianOS --storagectl IDE --port 0 --device 0 --medium none >nul 2>&1
