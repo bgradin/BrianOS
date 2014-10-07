b9c52627d20b740add48d332d080bea7a25d1598
gradinafrica <gradinafrica@yahoo.com>
2014-10-06 23:06:07 -0700

    Changed io.asm to support the new string include file

--- code/includes/io.asm
+++ code/includes/io.asm
@@ -1,3 +1,7 @@
+;;;; io.asm ;;;;
+; Requires:
+;   string.asm
+
 ; PrintString
 ;   Prints a string
 ;   Inputs:
@@ -9,19 +13,10 @@ PrintString:
 push ebp
 push ecx
 push ebx
-push edi
 push eax
 push eax
 
-mov ecx, 0x00
-not ecx			; ecx = -1 or 4,294,967,295
-
-lea di, [eax]		; Load the address of the message
-mov al, 0x00
-cld			; Clear the direction flag
-repne scasb		; Find value in al (ecx = -strlen - 2)
-not ecx			; ecx = strlen + 1
-dec ecx			; ecx = strlen
+call StringLength
 
 mov ah, 0x13		; int 10/ah = 13h -> display string
 mov al, 0x01		; Write mode
@@ -31,7 +26,6 @@ pop ebp			; Pointer to string
 int 0x10		; Print message
 
 pop eax
-pop edi
 pop ebx
 pop ecx
 pop ebp
