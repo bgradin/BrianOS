cf0a3698071ef4754fbd229594c15a9f6958a4b8
gradinafrica <gradinafrica@yahoo.com>
2014-10-06 23:06:29 -0700

    Added a subroutine to print hex values to io.asm

--- code/includes/io.asm
+++ code/includes/io.asm
@@ -30,3 +30,50 @@ pop ebx
 pop ecx
 pop ebp
 ret
+
+; PrintHex
+;   Prints a hex value
+;   Inputs:
+;     eax: Hex value
+;     bl: Color attribute
+;     dh: Row
+;     dl: Column
+PrintHex:
+push eax
+push ebx
+push ecx
+push edx
+
+mov byte [hex_value+0], 48	; Store '0'
+mov byte [hex_value+1], 120	; Store 'x'
+
+mov edx, 2
+
+PrintHex_loop:
+mov ecx, eax
+and ecx, 0xF0000000	; Store highest four bits in ecx
+shr ecx, 28		; Align bits right
+
+mov ebx, [hex_table+ecx]; Get value from hex_table
+mov byte [hex_value+edx], bl; Store character between '0' and 'F'
+
+inc edx
+shl eax, 4
+jnz PrintHex_loop
+
+mov ecx, 0x00
+mov byte [hex_value+edx], 0; Store null byte on end
+
+pop edx
+pop ecx
+pop ebx
+
+mov eax, hex_value
+
+call PrintString
+
+pop eax
+ret
+
+hex_value: resb 11
+hex_table: db '0123456789ABCDEF'
